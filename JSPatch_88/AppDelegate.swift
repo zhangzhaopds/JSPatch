aba63bf2aa7a319abb41bf31edad82a2dc8c24bc
zhangzhaopds <zhangzhaopds@foxmail.com>
2016-10-17 14:56:59 +0800

    JSPatch热修复

--- JSPatch_88/AppDelegate.swift
+++ JSPatch_88/AppDelegate.swift
@@ -16,9 +16,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+
+        do {
+            JPEngine.start()
+            let sourcePath = Bundle.main.path(forResource: "main", ofType: "js")
+            let sc =
+            try String(contentsOfFile: sourcePath!, encoding:
+            String.Encoding.utf8)
+            
+            JPEngine.evaluateScript(sc)
+        } catch{
+            
+        }
+//        JPEngine.evaluateScript("console.log('JSPatch success');")
+        
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        let vc = ViewController()
+        let navi = UINavigationController.init(rootViewController: vc)
+        window?.rootViewController = navi
+        window?.makeKeyAndVisible()
+        
+        
+        
         return true
     }
-
+    
+   
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
---
+++ JSPatch_88/Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  JSPatch_88
+//
+//  Created by 张昭 on 2016/10/13.
+//  Copyright © 2016年 张昭. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import "JPEngine.h"
+
+
+#endif /* Header_h */
